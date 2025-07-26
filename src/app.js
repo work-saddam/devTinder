417dfd3c20803c3924c79e20a326c49cf165fbb6
Saddam Hussein <work.saddam786@gmail.com>
2025-07-26 23:28:40 +0530

    handling multiple routes, playing with res.send and next funcion

--- src/app.js
+++ src/app.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.send("Test");
-});
-app.use("/hello", (req, res) => {
-  res.send("hello");
-});
+app.get('/users', (req,res, next) => {
+  console.log("Handle 1")
+  // res.send("Handle1")
+  next()
+},(req,res,next) => {
+  console.log("Handle 2")
+  // res.send("Heading2")
+  next()
+},
+(req,res) => {
+  console.log("Handle 3")
+  res.send("Heading3")
+})
 
 app.listen(7777, () => {
   console.log(`Server is listening on http://localhost:7777`);

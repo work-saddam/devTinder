9fe4d3592b0242e7c1d717bf7e025962a0e5557a
Saddam Hussein <work.saddam786@gmail.com>
2025-07-29 10:56:44 +0530

    add signup api

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
-require("dotenv").config()
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const connectDB = require("./config/database");
+const User = require("./models/user");
+
+//Convert JSON in js Object
+//Middleware parses incoming requests with JSON payloads and makes the data available on req.body.
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).send("user created successfully");
+  } catch (error) {
+    res.status(400).send("something went wrog");
+  }
+});
 
 //connect database & listening server
 connectDB()

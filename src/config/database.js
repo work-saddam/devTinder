5f4cc3c6a3de49feead01adfdfaa7180b1f47b53
Saddam Hussein <work.saddam786@gmail.com>
2025-07-29 07:36:54 +0530

    add .env file and configure

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,8 @@
 const mongoose = require("mongoose");
 
-const URI =
-    "mongodb+srv://saddamwork:saddam@cluster0.4wzeg.mongodb.net/devTinder?retryWrites=true&w=majority&appName=Cluster0";  //mongoDB Atlas
-
-//   "mongodb://127.0.0.1:27017/devTinder?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.4.0";    //mongosh local
-
+    
 const connectDB = async () => {
-  await mongoose.connect(URI);
+  await mongoose.connect(process.env.MONGO_URI);
 };
 
 module.exports = connectDB;

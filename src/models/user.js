3b23678267f86a1e079d4d526f0129f35fcd16a8
Saddam Hussein <work.saddam786@gmail.com>
2025-07-30 09:11:17 +0530

    add DB level validation

--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,78 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const userSchema = mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxlength: 10,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      trim: true,
+      maxLength: 20,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalif Email " + value);
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Enter a Strong Password");
+        }
+      },
+    },
+    age: {
+      type: Number,
+      min: 15,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "other"].includes(value)) {
+          throw new Error("Gender data is not valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG.png",
+      vallidate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid URL");
+        }
+      },
+    },
+    about: {
+      type: String,
+      maxLength: 50,
+    },
+    skills: {
+      type: [String],
+      validate(value) {
+        if (value.length > 10) {
+          throw new Error("Skills should not be more than 10");
+        }
+      },
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailID: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 // Model name should always be capital.
 const User = mongoose.model("User", userSchema);

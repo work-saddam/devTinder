e5bd4054ee082195de8e83982f904973e9835377
Saddam Hussein <work.saddam786@gmail.com>
2025-07-31 17:52:15 +0530

    add change password api

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const profileRouter = express.Router();
-
 const { userAuth } = require("../middlewares/auth");
 const { validateEditProfileData } = require("../utils/validation");
 const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 // Get Profile
 profileRouter.get("/profile/view", userAuth, async (req, res) => {
@@ -32,6 +32,32 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
     });
   } catch (error) {
     res.status(400).send("ERROR: " + error.message);
+  }
+});
+
+// Change password
+profileRouter.patch("/profile/password", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const { currentPassword, newPassword } = req.body;
+    if (!currentPassword || !newPassword)
+      throw new Error("all fields are required");
+
+    const isPasswordValid = await loggedInUser.validatePassword(currentPassword);
+    if (!isPasswordValid) throw new Error("Incorrect Password");
+
+    const isStrongPassword = validator.isStrongPassword(newPassword)
+    if(!isStrongPassword) throw new Error("Please enter strong password")
+
+    const passwordHash = await bcrypt.hash(newPassword, 10);
+
+    loggedInUser.password = passwordHash;
+    await loggedInUser.save();
+
+    res.send("Password updated successfully");
+  } catch (error) {
+    res.send("ERROR: " + error.message);
   }
 });
 

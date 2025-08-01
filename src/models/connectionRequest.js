af84224191ebb0f325b6255271e3e7c3b3f502f4
Saddam Hussein <work.saddam786@gmail.com>
2025-08-01 07:18:00 +0530

    add coonectionRequest model

---
+++ src/models/connectionRequest.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const connectionRequestSchema = new mongoose.Schema(
+  {
+    fromUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      require: true,
+    },
+    toUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      require: true,
+    },
+    status: {
+      type: String,
+      require: true,
+      enum: {
+        values: ["ignored", "interested", "accepted", "rejected"],
+        message: `{VALUE} is incorrect status type`,
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+// By adding index, our query will be very fast.
+connectionRequestSchema.index({ fromUserId: 1, toUserId: 1 });
+
+connectionRequestSchema.pre("save", function (next) {
+  const connectionRequest = this;
+  // Check if fromUserId is same to toUserId
+  if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
+    throw new Error("Cannot send connection request to yourself!");
+  }
+  next();
+});
+
+// Model name should always be capital.
+const ConnectionRequest = new mongoose.model(
+  "ConnectionRequest",
+  connectionRequestSchema
+);
+module.exports = ConnectionRequest;

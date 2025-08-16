337225e2acd15c4c12f90aa8806b974fc5fe5baf
Saddam Hussein <work.saddam786@gmail.com>
2025-08-16 08:44:16 +0530

    hashing roomId

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,4 +1,12 @@
 const socket = require("socket.io");
+const crypto = require("crypto");
+
+const getSecretRoomId = (userId, targetUserId) => {
+  return crypto
+    .createHash("sha256")
+    .update([userId, targetUserId].sort().join("_"))
+    .digest("hex");
+};
 
 const initializeSocket = (server) => {
   const io = socket(server, {
@@ -10,13 +18,13 @@ const initializeSocket = (server) => {
   io.on("connection", (socket) => {
     //Handle Events
     socket.on("joinChat", ({ firstName, userId, targetUserId }) => {
-      const roomId = [userId, targetUserId].sort().join("_");
-      // console.log(firstName + " Join room: " + roomId);
+      const roomId = getSecretRoomId(userId, targetUserId);
+      // console.log(firstName + " Joins room: " + roomId);
       socket.join(roomId);
     });
 
     socket.on("sendMessage", ({ firstName, userId, targetUserId, text }) => {
-      const roomId = [userId, targetUserId].sort().join("_");
+      const roomId = getSecretRoomId(userId, targetUserId);
       // console.log(firstName + ": " + text);
       io.to(roomId).emit("messageRecieved", { firstName, text });
     });

07018efd8d3d5cd7eae2c28d1436def20f93ccc8
Saddam Hussein <work.saddam786@gmail.com>
2025-08-19 11:24:51 +0530

    fix: socket cors issue

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -12,7 +12,8 @@ const getSecretRoomId = (userId, targetUserId) => {
 const initializeSocket = (server) => {
   const io = socket(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: "https://devtinder-chi.vercel.app",
+      Credential: true,
     },
   });
 
